cec726955e9b0a9b865a0c448bab84e63c7e24f7
AdamSgier <adam.sgier1@gmail.com>
2021-07-29 14:10:23 +0300

    Deploy flask app 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask.globals import request
 from flask.json import jsonify
 from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
@@ -11,19 +11,27 @@ from clarifai_grpc.grpc.api.status import status_code_pb2
 
 
 import base64
+
+
+
+
 with open("C:/Users/adams/Downloads/Eq_it-na_pizza-margherita_sep2005_sml.jpg", "rb") as img_file:
     my_string = base64.b64encode(img_file.read())
 
-import base64
-imgdata = base64.b64decode(my_string)
 filename = 'pizza.jpg'  # I assume you have a way of picking unique filenames
-with open(filename, 'wb') as f:
-    f.write(imgdata)
 
 
-def convert(base64):
-    with open(base64, "rb") as f:
+
+def convert(my_string):
+    imgdata = base64.b64decode(my_string)
+    print(imgdata)
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
+    with open(filename, "rb") as f:
         file_bytes = f.read()
+
+    print(file_bytes)
+    print(1)
     metadata = (('authorization', 'Key 69afe5ce2e5246f8b733fd3213dd7d33'),)
     post_model_outputs_response = stub.PostModelOutputs(
         service_pb2.PostModelOutputsRequest(
@@ -43,7 +51,7 @@ def convert(base64):
 
     if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
         raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
-    
+
     # Since we have one input, one output will exist here.
     output = post_model_outputs_response.outputs[0]
     print(output.data.concepts[0].name)
@@ -53,9 +61,16 @@ def convert(base64):
 app = Flask(__name__)
 
 
+@app.route('/')
+def main():
+    return render_template("main.html")
+
+
 @app.route('/<img>', methods=['GET', 'POST'])
 def index(img):
+    img = img.replace('-', '/')
     result = convert(img)
+    print(result)
     return {"food": result}
 
 
